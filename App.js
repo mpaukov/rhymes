533e676eaf61c6125da6437ccd9dec840a8844c2
Michael Paukov <michael.paukov@gmail.com>
2023-03-06 14:47:27 +0100

    test

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+
+//Q0MvyX1PMWVSzIpnF2ijiOszTcQgtUx8tHjHAiCk05RuYCiluzEy9450I1bO1exV
+
+axios.defaults.headers.common["api-key"] =
+  "Q0MvyX1PMWVSzIpnF2ijiOszTcQgtUx8tHjHAiCk05RuYCiluzEy9450I1bO1exV";
 
 export default function App() {
+  const response = async () => {
+    return await axios({
+      method: "post",
+      url: "https://eu-central-1.aws.data.mongodb-api.com/app/data-bmyij/endpoint/data/v1/action/findOne",
+      data: {
+        collection: "rhymes",
+        database: "books",
+        dataSource: "Cluster0",
+      },
+    }).then((res) => res.data.document.text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>test</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
