cf378fb7ffadf02aeee91dd6907154f05c523cfc
Michael Paukov <michael.paukov@gmail.com>
2023-03-08 14:04:50 +0100

    add screens

--- App.js
+++ App.js
@@ -1,68 +1,72 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, SafeAreaView, FlatList, View, Text } from "react-native";
-import axios from "axios";
-import { API_KEY, DB_URL } from "@env";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, Entypo, MaterialIcons } from "@expo/vector-icons";
+import MainScreen from "./screens/MainScreen";
+import NurseryRhymesScreen from "./screens/NurseryRhymesScreen";
+import RhymesScreen from "./screens/RhymesScreen";
+import AudioScreen from "./screens/AudioScreen";
+import NurseryRhymesExercisesScreen from "./screens/NurseryRhymesExercisesScreen";
 
-axios.defaults.headers.common["api-key"] = API_KEY;
-
-const response = async () => {
-  return await axios({
-    method: "post",
-    url: `${DB_URL}/action/find`,
-
-    data: {
-      collection: "rhymes",
-      database: "books",
-      dataSource: "Cluster0",
-      filter: { author: "Агния Барто" },
-    },
-  }).then((res) => res.data.documents);
-};
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    (async () =>
-      await response().then((data) =>
-        setData(data.sort((a, b) => a.text.length - b.text.length))
-      ))();
-  }, []);
-
-  const Item = ({ title, text }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.text}>{text}</Text>
-    </View>
-  );
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item title={item.title} text={item.text} />}
-        keyExtractor={(_, idx) => idx}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ tabBarShowLabel: false, headerShown: false }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+          name="Main"
+          component={MainScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Entypo name="open-book" size={size} color={color} />
+            ),
+          }}
+          name="Nursery"
+          component={NurseryRhymesScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialIcons
+                name="sports-basketball"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+          name="Exercises"
+          component={NurseryRhymesExercisesScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <Entypo name="book" size={size} color={color} />
+            ),
+          }}
+          name="Rhymes"
+          component={RhymesScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialIcons name="audiotrack" size={size} color={color} />
+            ),
+          }}
+          name="Audio"
+          component={AudioScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 32,
-  },
-  text: {
-    fontSize: 24,
-  },
-});
---
+++ screens/NurseryRhymesExercisesScreen.js
@@ -0,0 +1,71 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, FlatList, View, Text } from "react-native";
+import axios from "axios";
+import { API_KEY, DB_URL } from "@env";
+
+axios.defaults.headers.common["api-key"] = API_KEY;
+
+const response = async () => {
+  return await axios({
+    method: "post",
+    url: `${DB_URL}/action/find`,
+
+    data: {
+      collection: "exercises",
+      database: "books",
+      dataSource: "Cluster0",
+    },
+  }).then((res) => res.data.documents);
+};
+
+export default function NurseryRhymesExercisesScreen() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () =>
+      await response().then((data) =>
+        setData(data.sort((a, b) => a.text.length - b.text.length))
+      ))();
+  }, []);
+
+  const Item = ({ title, text }) => (
+    <View style={styles.item}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.mainTitle}>Играем и делаем упражнения</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item title={item.title} text={item.text} />}
+        keyExtractor={(_, idx) => idx}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+  },
+  mainTitle: {
+    fontSize: 32,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 32,
+  },
+  text: {
+    fontSize: 24,
+  },
+});
